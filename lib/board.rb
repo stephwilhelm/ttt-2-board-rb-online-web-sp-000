90c21c8b0c6680374e2d0ad53ab78ae0ebe29e40
sunny-mouse-7530 <wilhelm.steph@gmail.com>
2019-10-21 23:24:48 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,30 @@
-# Define the variable board below.
+board = "Tic Tac Toe Board"
+
+board = [" "," "," "," "," "," "," "," "," "]# Define the variable board below.
+
+# Position 1: left
+board[0]
+
+# Position 2: Middle
+board[1]
+
+# Position 3: right
+board[2]
+
+# Position 4: middleleft
+board[3]
+
+# Position 5: middleMiddle
+board[4]
+
+# Position 6: middleright
+board[5]
+
+# Position 7: bottomleft
+board[6]
+
+# Position 7: bottomMiddle
+board[7]
+
+# Position 9: bottomright
+board[8]
