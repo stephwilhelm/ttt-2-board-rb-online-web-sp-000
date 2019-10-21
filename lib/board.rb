1ce19b11562c0bab56e619c540b6797667032228
sunny-mouse-7530 <wilhelm.steph@gmail.com>
2019-10-21 23:07:17 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,28 @@
 row = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]# Define the variable board below.
+
+# Position 1: left
+row[0]
+
+# Position 2: Middle
+row[1]
+
+# Position 3: right
+row[2]
+
+# Position 4: middleleft
+row[3]
+
+# Position 5: middleMiddle
+row[4]
+
+# Position 6: middleright
+row[5]
+
+# Position 7: bottomleft
+row[6]
+
+# Position 7: bottomMiddle
+row[7]
+
+# Position 9: bottomright
+row[8]
