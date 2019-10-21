2f2896a411758a3bdac474df3abfab2e0d1758aa
sunny-mouse-7530 <wilhelm.steph@gmail.com>
2019-10-21 23:22:29 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,30 +1,30 @@
 board = "Tic Tac Toe Board"
 
-row = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]# Define the variable board below.
+board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]# Define the variable board below.
 
 # Position 1: left
-row[0]
+board[0]
 
 # Position 2: Middle
-row[1]
+board[1]
 
 # Position 3: right
-row[2]
+board[2]
 
 # Position 4: middleleft
-row[3]
+board[3]
 
 # Position 5: middleMiddle
-row[4]
+board[4]
 
 # Position 6: middleright
-row[5]
+board[5]
 
 # Position 7: bottomleft
-row[6]
+board[6]
 
 # Position 7: bottomMiddle
-row[7]
+board[7]
 
 # Position 9: bottomright
-row[8]
+board[8]
